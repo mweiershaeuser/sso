41cf395d9b976160f97948fa56b434ea17e8958c
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-09-01 19:14:21 +0200

    feat: added environment setup and first login draft

---
+++ auth/src/app/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function login(formData: FormData) {
+  const cookieStore = cookies();
+
+  const user = formData.get("user");
+  const password = formData.get("password");
+
+  const response = await fetch(`${process.env.ZITADEL_HOST}/v2beta/sessions`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.ZITADEL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      checks: {
+        user: {
+          loginName: user,
+        },
+        password: {
+          password: password,
+        },
+      },
+    }),
+  });
+
+  const session = await response.json();
+
+  cookieStore.set("sessionId", session.sessionId, { httpOnly: true });
+  cookieStore.set("sessionToken", session.sessionToken, { httpOnly: true });
+}
--- auth/src/app/page.tsx
+++ auth/src/app/page.tsx
@@ -1,3 +1,30 @@
+import { login } from "./actions";
+
 export default function Home() {
-  return <main className="min-h-screen">Hallo</main>;
+  return (
+    <main className="min-h-screen">
+      <h1 className="mb-2">Login</h1>
+      <form action={login} className="flex flex-col">
+        <label className="input mb-2">
+          <input type="text" name="user" placeholder="Benutzername" required />
+        </label>
+
+        <label className="input mb-2">
+          <input
+            type="password"
+            name="password"
+            placeholder="Passwort"
+            required
+          />
+        </label>
+
+        <input
+          type="submit"
+          role="button"
+          className="btn btn-primary"
+          value="Anmelden"
+        />
+      </form>
+    </main>
+  );
 }
