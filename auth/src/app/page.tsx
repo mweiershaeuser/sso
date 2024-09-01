63cedb712eb59da6169f31ce2c9f98b13ab37196
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-09-02 00:24:12 +0200

    feat: pipeline test

--- auth/src/app/page.tsx
+++ auth/src/app/page.tsx
@@ -3,7 +3,7 @@ import { login } from "./actions";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <h1 className="mb-2">Login</h1>
+      <h1 className="mb-2">Auth</h1>
       <form action={login} className="flex flex-col">
         <label className="input mb-2">
           <input type="text" name="user" placeholder="Benutzername" required />
