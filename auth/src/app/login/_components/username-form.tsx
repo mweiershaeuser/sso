769ba232f7b0a1d4f39fc9a9c0dbdede1c1f5fc5
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-09-27 22:05:32 +0200

    feat: added required info to username form

--- auth/src/app/login/_components/username-form.tsx
+++ auth/src/app/login/_components/username-form.tsx
@@ -13,47 +13,60 @@ export default function UsernameForm() {
   }, [state]);
 
   return (
-    <form action={formAction} className="flex flex-col">
-      <label className="form-control">
-        <div className="label">
-          <span className={`label-text ${state?.errors?.user && "text-error"}`}>
-            Benutzername
-          </span>
-        </div>
-        <input
-          type="text"
-          name="user"
-          ref={userRef}
-          className={`input input-bordered ${state?.errors?.user && "input-error"}`}
-          autoComplete="username"
-          aria-invalid={!!state?.errors?.user}
-          aria-describedby="userError"
-        />
-      </label>
-      {state?.errors?.user && (
-        <p id="userError" className="m-0 text-error">
-          <i
-            className="bi bi-exclamation-circle-fill"
-            aria-label="Fehler: "
-            role="img"
-          />{" "}
-          {state?.errors?.user}
-        </p>
-      )}
-
-      <div className="form-control mt-3">
-        <label className="label cursor-pointer">
-          <span className="label-text">Angemeldet bleiben</span>
-          <input type="checkbox" name="stayLoggedIn" className="checkbox" />
+    <>
+      <p aria-hidden>
+        <i
+          className="bi bi-info-circle-fill"
+          aria-label="Info: "
+          role="img"
+        ></i>{" "}
+        Pflichtfelder sind mit * markiert.
+      </p>
+      <form action={formAction} className="flex flex-col">
+        <label className="form-control">
+          <div className="label">
+            <span
+              className={`label-text ${state?.errors?.user && "text-error"}`}
+            >
+              Benutzername <span aria-hidden>*</span>
+            </span>
+          </div>
+          <input
+            type="text"
+            name="user"
+            ref={userRef}
+            className={`input input-bordered ${state?.errors?.user && "input-error"}`}
+            autoComplete="username"
+            aria-required
+            aria-invalid={!!state?.errors?.user}
+            aria-describedby="userError"
+          />
         </label>
-      </div>
+        {state?.errors?.user && (
+          <p id="userError" className="m-0 text-error">
+            <i
+              className="bi bi-exclamation-circle-fill"
+              aria-label="Fehler: "
+              role="img"
+            ></i>{" "}
+            {state?.errors?.user}
+          </p>
+        )}
 
-      <input
-        type="submit"
-        role="button"
-        className="btn btn-primary mt-3"
-        value="Anmelden"
-      />
-    </form>
+        <div className="form-control mt-3">
+          <label className="label cursor-pointer">
+            <span className="label-text">Angemeldet bleiben</span>
+            <input type="checkbox" name="stayLoggedIn" className="checkbox" />
+          </label>
+        </div>
+
+        <input
+          type="submit"
+          role="button"
+          className="btn btn-primary mt-3"
+          value="Anmelden"
+        />
+      </form>
+    </>
   );
 }
