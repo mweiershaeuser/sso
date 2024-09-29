414e180a0e725dd15732ea1a0385b0e98225c2d4
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-09-29 13:39:51 +0200

    feat: added explanation to username display

--- auth/src/app/login/_components/username-display.tsx
+++ auth/src/app/login/_components/username-display.tsx
@@ -7,33 +7,36 @@ import { selectUser } from "@/store/user/userSlice";
 export default function UsernameDisplay() {
   const { username, givenName, familyName } = useAppSelector(selectUser);
   return (
-    <div className="flex items-center justify-between bg-base-100 rounded-lg shadow p-4">
-      <div className="flex items-center gap-5">
-        <div className="avatar placeholder">
-          <div className="bg-neutral text-neutral-content w-12 h-12 rounded-full">
-            <span className="text-lg">
-              {givenName[0]?.toUpperCase()}
-              {familyName[0]?.toUpperCase()}
-            </span>
+    <>
+      <p className="mb-2">Anmelden als:</p>
+      <div className="flex items-center justify-between bg-base-100 rounded-lg shadow p-4">
+        <div className="flex items-center gap-5">
+          <div className="avatar placeholder">
+            <div className="bg-neutral text-neutral-content w-12 h-12 rounded-full">
+              <span className="text-lg">
+                {givenName[0]?.toUpperCase()}
+                {familyName[0]?.toUpperCase()}
+              </span>
+            </div>
+          </div>
+          <div>
+            <p className="text-2xl m-0">{`${givenName} ${familyName}`}</p>
+            <p className="italic m-0">{username}</p>
           </div>
         </div>
         <div>
-          <p className="text-2xl m-0">{`${givenName} ${familyName}`}</p>
-          <p className="italic m-0">{username}</p>
+          <button
+            className="btn btn-circle btn-outline"
+            onClick={() => deleteSession()}
+          >
+            <i
+              className="bi bi-x-lg"
+              aria-label="Benutzer:in ändern"
+              role="img"
+            ></i>
+          </button>
         </div>
       </div>
-      <div>
-        <button
-          className="btn btn-circle btn-outline"
-          onClick={() => deleteSession()}
-        >
-          <i
-            className="bi bi-x-lg"
-            aria-label="Benutzer:in ändern"
-            role="img"
-          ></i>
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
