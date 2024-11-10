9e0f0b9b97f95598402d68ade86a9e7f9ecae22c
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-11-10 21:05:55 +0100

    feat: added link title to logo link

--- auth/src/app/_components/header.tsx
+++ auth/src/app/_components/header.tsx
@@ -1,13 +1,20 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import AuthWrapper from "./auth-wrapper";
 import LocaleSwitch from "./locale-switch";
 
 export default function Header() {
+  const t = useTranslations("RootLayout.Header");
+
   return (
     <header className="navbar w-full rounded-lg bg-base-200">
       <div className="navbar-start">
         <div className="font-display">
-          <Link href="/" className="btn btn-ghost text-2xl">
+          <Link
+            href="/"
+            className="btn btn-ghost text-2xl"
+            title={t("logoLinkTitle")}
+          >
             <span className="font-light">mw</span>
             <span className="font-bold max-[450px]:hidden">auth</span>
           </Link>
