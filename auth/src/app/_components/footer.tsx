e08b6ac40c9370e162f1038f216db181c97e7880
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-11-10 21:03:11 +0100

    feat: adjusted Button and Link labels and added link title to footer link

--- auth/src/app/_components/footer.tsx
+++ auth/src/app/_components/footer.tsx
@@ -1,4 +1,7 @@
+import { useTranslations } from "next-intl";
+
 export default function Footer() {
+  const t = useTranslations("RootLayout.Footer");
   return (
     <footer className="footer w-full rounded-lg p-4 flex justify-center bg-base-200 text-base-content">
       <span className="inline" lang="en">
@@ -6,6 +9,7 @@ export default function Footer() {
         <i className="bi bi-heart-fill" aria-label="love" role="img"></i> by{" "}
         <a
           href="https://github.com/mweiershaeuser"
+          title={t("authorProfileLinkTitle")}
           target="_blank"
           className="link link-primary"
         >
