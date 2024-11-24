20ec95fea689c6586893f03f3b525a4ad312fff8
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-11-24 23:33:47 +0100

    feat: removed setting locale cookie automatically to comply with GDPR

--- auth/src/i18n/server-actions.ts
+++ auth/src/i18n/server-actions.ts
@@ -1,18 +1,7 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { defaultLocale, Locale } from "./models/locale";
-
-export async function initialiseLocale() {
-  const cookieStore = cookies();
-  if (!cookieStore.has("locale")) {
-    cookieStore.set("locale", defaultLocale, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-    });
-  }
-}
+import { Locale } from "./models/locale";
 
 export async function setLocale(locale: Locale) {
   const cookieStore = cookies();
