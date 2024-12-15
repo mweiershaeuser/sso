c3c128b4bd70ebd74bd6cb80186a05e7a4228592
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-12-15 03:20:10 +0100

    feat: added spacing for alerts

--- auth/src/app/_components/alerts.tsx
+++ auth/src/app/_components/alerts.tsx
@@ -8,19 +8,19 @@ export default function Alerts() {
   const alerts = useAppSelector(selectAlert);
 
   return (
-    <div className="px-4">
-      <div aria-live="polite">
+    <div className="flex flex-col gap-3 px-4">
+      <div className="flex flex-col gap-3" aria-live="assertive">
         {alerts
-          .filter((alert) => alert.role === "status")
+          .filter((alert) => alert.role === "alert")
           .map((alert) => (
             <Alert key={alert.id} type={alert.type}>
               <p className="m-0">{alert.content}</p>
             </Alert>
           ))}
       </div>
-      <div aria-live="assertive">
+      <div className="flex flex-col gap-3" aria-live="polite">
         {alerts
-          .filter((alert) => alert.role === "alert")
+          .filter((alert) => alert.role === "status")
           .map((alert) => (
             <Alert key={alert.id} type={alert.type}>
               <p className="m-0">{alert.content}</p>
