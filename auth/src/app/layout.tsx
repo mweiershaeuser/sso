625b282591fb21ce9a809a649b0c5b86ffe55879
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-12-15 03:11:15 +0100

    feat: removed react-hot-toast

--- auth/src/app/layout.tsx
+++ auth/src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata, Viewport } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages, getTranslations } from "next-intl/server";
 import { MuseoModerno, Open_Sans } from "next/font/google";
-import { Toaster } from "react-hot-toast";
 import Alerts from "./_components/alerts";
 import Footer from "./_components/footer";
 import Header from "./_components/header";
@@ -55,36 +54,6 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <StoreProvider>
             <SkipLink label={t("skipToMainContentLink")} href="#main-content" />
-            <Toaster
-              position="top-center"
-              toastOptions={{
-                duration: 5000,
-                style: {
-                  color: "oklch(var(--ac))",
-                  background: "oklch(var(--a))",
-                },
-                success: {
-                  style: {
-                    color: "oklch(var(--suc))",
-                    background: "oklch(var(--su))",
-                  },
-                  iconTheme: {
-                    primary: "oklch(var(--suc))",
-                    secondary: "oklch(var(--su))",
-                  },
-                },
-                error: {
-                  style: {
-                    color: "oklch(var(--erc))",
-                    background: "oklch(var(--er))",
-                  },
-                  iconTheme: {
-                    primary: "oklch(var(--erc))",
-                    secondary: "oklch(var(--er))",
-                  },
-                },
-              }}
-            />
             <div className="w-full p-4">
               <Header />
             </div>
